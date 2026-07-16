dd00152e2de3cc2be4d4c94755438408ab56939e
Varelfebrianda <varelfebriandaputra@gmail.com>
2026-07-16 11:34:42 +0700

    del

--- src/features/tables/utils/tableStyles.ts
+++
@@ -1,7 +0,0 @@
-import type { Column } from "./types";
-
-export function getAlignClass(align?: Column<never>["align"]) {
-  if (align === "center") return "text-center";
-  if (align === "right") return "text-right";
-  return "";
-}
