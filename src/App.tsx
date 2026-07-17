065e6ccb27fabde243c02bd3d5cf39e74ca5741e
Varelfebrianda <varelfebriandaputra@gmail.com>
2026-07-17 14:30:40 +0700

    add css calc

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import LargeDataTable from './pages/LargeDataTable'
-import Jobvacation from './pages/Jobvacation'
+// import Jobvacation from './pages/Jobvacation'
 import "./styles/index.css"
 
 export default function App() {
   return (
     <div className="app-root-container">
-      <div className="page-section">
-        <LargeDataTable />
-      </div>
-      <div className="page-section">
-        <Jobvacation />
-      </div>
+      <LargeDataTable />
+      {/* <Jobvacation /> */}
     </div>
   )
 }
